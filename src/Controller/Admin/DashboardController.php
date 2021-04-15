f464a195fad16199b0691de38d3868ea4caf1b3a
Estelle-Rose <serenetzen@gmail.com>
2021-04-15 17:48:24 +0200

    création des produits et affichage

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -8,6 +8,7 @@ use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -18,7 +19,19 @@ class DashboardController extends AbstractDashboardController
      */
     public function index(): Response
     {
-        return parent::index();
+        // redirect to some CRUD controller
+        //$routeBuilder = $this->get(AdminUrlGenerator::class);
+
+       // return $this->redirect($routeBuilder->setController(ProductCrudController::class)->generateUrl());
+
+        // you can also redirect to different pages depending on the current user
+        if ('marc' === $this->getUser()->getUsername()) {
+            return $this->redirect('home');
+        }
+
+        // you can also render some template to display a proper Dashboard
+        // (tip: it's easier if your template extends from @EasyAdmin/page/content.html.twig)
+        return $this->render('admin/mon-bureau.html.twig');
     }
 
     public function configureDashboard(): Dashboard
