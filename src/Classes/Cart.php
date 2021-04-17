c64615a0459f51ffc8d46aac831b992414280859
Estelle-Rose <serenetzen@gmail.com>
2021-04-17 17:40:25 +0200

    optimisation du code du panier

--- src/Classes/Cart.php
+++ src/Classes/Cart.php
@@ -3,25 +3,30 @@
 
 namespace App\Classes;
 
+use App\Entity\Product;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
 
 class Cart
 {
     private $session;
-    public function __construct(SessionInterface $session)
+    private $entityManager;
+    public function __construct(SessionInterface $session,EntityManagerInterface $entityManager)
     {
         $this->session = $session;
+        $this->entityManager = $entityManager;
     }
 
     public function add($id)
     {
         $cart =$this->session->get('cart',[]);
+
         if(!empty($cart[$id])) {
             $cart[$id]++;
         } else {
             $cart[$id] = 1;
         }
-        $this->session->set('cart',$cart);
+        return $this->session->set('cart',$cart);
     }
     public function delete()
     {
@@ -31,12 +36,45 @@ class Cart
     {
         $cart = $this->session->get('cart',[]);
         unset($cart[$id]);
-        $this->session->set('cart',$cart);
-        return  $this->session->set('cart',$cart);;
+        return  $this->session->set('cart',$cart);
 
     }
     public function get()
     {
         return $this->session->get('cart');
+    }
+
+    public function getFull()
+    {
+        $cartComplete = [];
+
+
+        if($this->get()) {
+            foreach ($this->get() as $id => $quantity) {
+                $product = $this->entityManager->getRepository(Product::class)->findOneById($id);
+                if(!$product) { // si un user saisi une url avec id erroné on supprime le produit
+                    // qui n'existe pas dans la bdd du panier
+                    $this->remove($id);
+                    continue; // permet de sortir de la boucle
+                }
+                $cartComplete[]= [
+                    'product' =>  $product,
+                    'quantity' => $quantity
+                ];
+
+            }
+
+        }
+        return $cartComplete;
+    }
+    public function decrease($id)
+    {
+        $cart =$this->session->get('cart',[]);
+        if($cart[$id] >1){
+            $cart[$id]--;
+        } else {
+            unset($cart[$id]);
+        }
+        return $this->session->set('cart',$cart);
     }
 }
--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -5,7 +5,6 @@ namespace App\Controller;
 
 use App\Classes\Cart;
 
-use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -15,18 +14,11 @@ class CartController extends AbstractController
     /**
      * @Route("/mon-panier", name="cart")
      */
-    public function index(Cart $cart, ProductRepository $repo): Response
+    public function index(Cart $cart): Response
     {
-        $cartComplete = [];
 
-        foreach ($cart->get() as $id => $quantity) {
-            $cartComplete[]= [
-                'product' => $repo->findOneById($id),
-                'quantity' => $quantity
-            ];
-        }
         return $this->render('cart/index.html.twig', [
-            'cart' => $cartComplete
+            'cart' => $cart->getFull()
         ] );
     }
     /**
@@ -48,10 +40,18 @@ class CartController extends AbstractController
     /**
      * @Route("/mon-panier/delete", name="delete_cart")
      */
-    public function delete( Cart $cart): Response
+    public function deleteCart( Cart $cart): Response
     {
         $cart->delete();
         return $this->redirectToRoute('products');
     }
 
+    /**
+     * @Route("/mon-panier/decrease/{id}", name="decrease_quantity")
+     */
+    public function decreaseQuantity( Cart $cart, $id): Response
+    {
+        $cart->decrease($id);
+        return $this->redirectToRoute('cart');
+    }
 }
